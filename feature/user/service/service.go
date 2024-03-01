b2c3e8b7bb9321c5471cb87f851e41ee40665597
teranixbq <hanieffathulb03@gmail.com>
2024-03-01 22:19:18 +0700

    fix : 404, decode bind json

--- feature/user/service/service.go
+++ feature/user/service/service.go
@@ -2,18 +2,19 @@ package service
 
 import (
 	"errors"
+
 	"mlogreport/feature/user/dto/request"
 	"mlogreport/feature/user/dto/response"
 	"mlogreport/feature/user/repository"
 	"mlogreport/utils/auth"
+	"mlogreport/utils/constanta"
 	"mlogreport/utils/helper"
+	"mlogreport/utils/validation"
 )
 
 type userService struct {
 	userRepository repository.UserRepositoryInterface
 }
-
-
 
 type UserServiceInterface interface {
 	InsertUser(data request.RequestUser) error
@@ -29,6 +30,16 @@ func NewUserService(userRepository repository.UserRepositoryInterface) UserServi
 }
 
 func (user *userService) InsertUser(data request.RequestUser) error {
+	errEmpty := validation.CheckEmpty(data.Class, data.Name, data.Nim, data.Password)
+	if errEmpty != nil {
+		return errEmpty
+	}
+
+	errLength := validation.CheckLength(data.Password)
+	if errLength != nil {
+		return errLength
+	}
+
 	password, err := helper.HashPass(data.Password)
 	if err != nil {
 		return err
@@ -44,13 +55,18 @@ func (user *userService) InsertUser(data request.RequestUser) error {
 }
 
 func (user *userService) Login(data request.RequestLogin) (response.ResponseLogin, error) {
+	errEmpty := validation.CheckEmpty(data.Nim, data.Password)
+	if errEmpty != nil {
+		return response.ResponseLogin{}, errEmpty
+	}
+
 	dataUser, err := user.userRepository.FindNim(data.Nim)
 	if err != nil {
 		return response.ResponseLogin{}, err
 	}
 
 	if !helper.CompareHash(dataUser.Password, data.Password) {
-		return response.ResponseLogin{}, errors.New("error : password salah")
+		return response.ResponseLogin{}, errors.New(constanta.WRONG_PASS)
 	}
 
 	token, err := auth.CreateToken(dataUser.Id, "")
@@ -68,12 +84,10 @@ func (user *userService) SelectUserById(id string) (response.ProfileUser, error)
 		return response.ProfileUser{}, err
 	}
 	return dataUser, nil
-
 }
 
-
 func (user *userService) UpdateProfile(id string, data request.RequestUpdateProfile) error {
-	err := user.userRepository.UpdateProfile(id,data)
+	err := user.userRepository.UpdateProfile(id, data)
 	if err != nil {
 		return err
 	}
